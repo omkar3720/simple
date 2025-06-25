390fdebc65a3c02aff2f8d8b50e90988f4b56aa9
omkar3720 <omkarshinde2728@gmail.com>
2025-06-25 12:47:12 +0530

    add

---
+++ RandomClass/src/Test.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+
+public class Test 
+{
+	private final static Random r = new Random();
+	
+	public int createRandom()
+	{
+
+		   int i = r.nextInt(100000,999999);
+		
+		   return i;
+		
+	}
+   public static void main(String[] args) 
+   {
+	   Test t = new Test();
+	   int i1=t.createRandom();
+	   System.out.println(i1);
+	   
+}
+}
