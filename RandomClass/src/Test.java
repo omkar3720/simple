947f2031084561972e504fe304bc5382e93e488a
omkar3720 <omkarshinde2728@gmail.com>
2025-06-25 12:51:26 +0530

    add1

--- RandomClass/src/Test.java
+++ RandomClass/src/Test.java
@@ -8,10 +8,11 @@ public class Test
 	{
 
 		   int i = r.nextInt(100000,999999);
-		
+		    System.out.println("This is Random Class No Logic");
 		   return i;
 		
 	}
+	
    public static void main(String[] args) 
    {
 	   Test t = new Test();
